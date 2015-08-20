d13da46e090557ab7c2f587097088963771c4235
colemanval16 <colemanval16@gmail.com>
2015-08-20 09:25:19 +0100

    Vals Attempt

---
+++ SortExample/SortExample/ArrayExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortExample
+{
+    class ArrayExample
+    {
+        static void Main(string[] args)
+        {
+            //creating an array instance of size 5
+            int[] array = new int[5];
+
+            // creating an instance of Random clas
+            Random random = new Random();
+
+            //populating the array with random numbers between 1 and 100
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 100);
+            }
+
+            //Displaying unsorted array
+            Console.WriteLine("\n**************Unsorted Array****************");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ",i);
+            }
+
+           //Sorting array
+            Array.Sort(array);
+
+            //Displaying sorted array
+            Console.WriteLine("\n***************Sorted Array******************");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+
+            //REversing an array
+            Array.Reverse(array);
+
+            //Displaying reversed array
+            Console.WriteLine("\n***************Reversed Array******************");
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SortExample/SortExample/StudentTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortExample
+{
+    class StudentTest
+    {
+        static void Main()
+        {
+            List<Student> list = new List<Student>();
+            Student student1 = new Student();
+            student1.StudentId = 1;
+            student1.Name = "Marry";
+            student1.Module = "OOP1";
+            Student student2 = new Student();
+            student2.StudentId = 101;
+            student2.Name = "John";
+            student2.Module = "Programming";
+            Student student3 = new Student();
+            student3.StudentId = 3;
+            student3.Name = "Peter";
+            student3.Module = "Databases";
+            
+            //Adding student instances to list
+            list.Add(student1);
+            list.Add(student2);
+            list.Add(student3);
+
+
+            Console.WriteLine("*******Unsorted Student List*********");
+            foreach (Student student in list)
+            {
+                Console.WriteLine(student);
+            }
+
+            //SortByNameThenByID sorting = new SortByNameThenByID();
+            //list.Sort(sorting);
+
+            list.Sort(new SortByNameThenByID());
+
+            Console.WriteLine("*******Sorted Student List*********");
+            foreach (Student student in list)
+            {
+                Console.WriteLine(student);
+            }
+            Console.ReadLine();
+        }
+    }
+}
