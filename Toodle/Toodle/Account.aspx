32919456e4c64eccca13070fe46cd5a4f728a448
jungaJK <wjddk0929@gmail.com>
2015-09-11 11:34:02 +0100

    Added ITnews Page, Signin, Account page

---
+++ Toodle/Toodle/Account.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Toodle.Master" AutoEventWireup="true" CodeBehind="Account.aspx.cs" Inherits="Toodle.Account" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="headMeta" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="headLinks" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server" Visible="False">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="Slider" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="mainContent" runat="server">
+        <div class="jumbotron">
+  <h1>Hello <asp:Label ID="lblName" runat="server" Text=""></asp:Label></h1>
+ 
+</div>
+</asp:Content>
--- Toodle/Toodle/Toodle.Master.cs
+++ Toodle/Toodle/Toodle.Master.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Toodle
 {
@@ -13,5 +17,58 @@ namespace Toodle
         {
 
         }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ToodleConnection = WebConfigurationManager.ConnectionStrings["ToodleDB"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(ToodleConnection))
+                {
+                    using (SqlCommand cmd = new SqlCommand("spLogIn", con))
+                    {
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        int id = 0;
+                        bool a = Int32.TryParse(txtStudentID.Text, out id);
+                        string password = txtPassword.Text;
+                        ////int id = Convert.ToInt32(txtStudentID.Text);
+                        //SqlParameter idParam = new SqlParameter("@studentID", SqlDbType.Int);
+                        //idParam.Value = a;
+                        //cmd.Parameters.Add(idParam);
+
+                        //SqlParameter passwrodParam = new SqlParameter("@password", SqlDbType.VarChar);
+                        //passwrodParam.Value = password;
+                        //cmd.Parameters.Add(passwrodParam);
+                        cmd.Parameters.AddWithValue("@studentID", id);
+                        cmd.Parameters.AddWithValue("@password", password);
+                        con.Open();
+
+                        SqlDataReader rd = cmd.ExecuteReader();
+                        txtPassword.Text = "connected";
+ 
+                            if (rd.Read())
+                            {
+                                txtPassword.Text = "";
+                                txtStudentID.Text = "";
+                                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "aaa", "alert('LOGIN OK')", true);
+                                useraccount.Visible = true;
+                                loginBox.Visible = false;
+                                lblName.Text = "Hello " +rd.GetValue(0).ToString();
+                            }
+                            else
+                            {
+                                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "aaa", "alert('LOGIN fail')", true);
+                            }
+                    }
+                }
+
+            }
+            catch (SqlException)
+            {
+
+            }
+        }
+
+        }
     }
-}
