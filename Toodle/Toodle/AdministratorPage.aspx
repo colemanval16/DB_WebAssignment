defe2246089299e1458187a9eb5a08aa4682820e
jungaJK <wjddk0929@gmail.com>
2015-09-14 17:51:13 +0100

    commit for update

--- Toodle/Toodle/AdministratorPage.aspx
+++ Toodle/Toodle/AdministratorPage.aspx
@@ -35,8 +35,29 @@
            </table>
         </div>
     </div>
-<br />
-<br />
-<br />
+        <div class="col-md-6">
+             <br /> 
+
+             <h4>Course Enrolment Status</h4>
+             <asp:Label ID="Label1" runat="server" Text="Please select course"></asp:Label>
+             <asp:DropDownList ID="DropDownList1" runat="server">
+                 <asp:ListItem Value="MTA01">MTA- Database</asp:ListItem>
+                 <asp:ListItem Enabled="False">MTA - Window Operating System</asp:ListItem>
+             </asp:DropDownList>
+             <asp:Button ID="Button1" class="btn btn-info text-center" runat="server" Text="SHOW" OnClick="btnShow_Click" />
+            <table class="table table-hover" id="Table1" runat="server" visible="False">
+                <tr>
+                    <th>Date</th>
+                    <th>Number of Student</th>
+                </tr>
+           </table>
+        </div>
+
+        <div class="row">
+	    <div class="col-md-3">       
+           <br /> 
+            <img src="Images/settingIcon.png" class="img-responsive accountImage" alt="student Info Icon"/>  
+        </div>
+    </div>
     </div>
 </asp:Content>
--- Toodle/Toodle/AdministratorPage.aspx.designer.cs
+++ Toodle/Toodle/AdministratorPage.aspx.designer.cs
@@ -47,5 +47,41 @@ namespace Toodle {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlTable courseStatusTable;
+        
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Table1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlTable Table1;
     }
 }
